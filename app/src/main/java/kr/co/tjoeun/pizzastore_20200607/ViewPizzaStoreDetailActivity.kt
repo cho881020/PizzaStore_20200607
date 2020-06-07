f2c45af2806504d5f30362fb19d819cc0496bd5b
cho881020 <kj_cho@idealidea.co.kr>
2020-06-07 16:22:40 +0900

    권한 확인 / 전화 연결 (CALL)

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
@@ -1,8 +1,13 @@
 package kr.co.tjoeun.pizzastore_20200607
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
 import kr.co.tjoeun.pizzastore_20200607.datas.PizzaStore
 
@@ -18,6 +23,31 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+//            권한을 먼저 확인하고 => OK이면 전화 연결
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    실제 전화 거는 코드
+                    val myUri = Uri.parse("tel:${mPizzaStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                }
+
+            }
+
+//            실제 권한 체크 실행
+            TedPermission.with(mContext)
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 
