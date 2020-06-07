56abe8a03aa02c1bfe859bcf5f0906d48444b361
cho881020 <kj_cho@idealidea.co.kr>
2020-06-07 16:16:06 +0900

    가게 상세 정보 반영

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200607/ViewPizzaStoreDetailActivity.kt
@@ -2,8 +2,13 @@ package kr.co.tjoeun.pizzastore_20200607
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
+import kr.co.tjoeun.pizzastore_20200607.datas.PizzaStore
 
 class ViewPizzaStoreDetailActivity : BaseActivity() {
+
+    lateinit var mPizzaStore : PizzaStore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,16 @@ class ViewPizzaStoreDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//        Serializable 형태로 첨부된 가게 정보를 받아서 => PizzaStore 형태로 변신 (Casting)시켜서
+//        멤버변수로 있는 피자가게 변수에 대입.
+        mPizzaStore = intent.getSerializableExtra("store") as PizzaStore
+
+//        대입 된 피자 가게 변수의 데이터들을 이용해서 화면에 출력
+
+        Glide.with(mContext).load(mPizzaStore.logoUrl).into(logoImg)
+        storeNameTxt.text = mPizzaStore.name
+        phoneNumTxt.text = mPizzaStore.phoneNum
+
 
     }
 
